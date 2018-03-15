8e02bf71a449e034891cb21c2b7bb3e8f6a2783f
AlliedToasters <michael.r.klear@gmail.com>
2018-03-15 16:40:19 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(name='pycda',
     version='0.1.0',
     description='Python Crater Detection Algorithm (PyCDA) is a pipeline for crater detection; go from image to annotated crater stats in minutes.',
     url='https://github.com/AlliedToasters/PyCDA',
+    kerywords = ['crater detection', 'astronomy', 'planetary science', 'planetary geology']
     author='Michael Klear',
     author_email='michael.klear@colorado.edu',
     license='MIT',
     packages=['pycda'],
+    classifiers=[],
     zip_safe=False
 )
