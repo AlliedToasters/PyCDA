34c8eb2366431155f147802046fd6e934ff2a641
AlliedToasters <michael.r.klear@gmail.com>
2018-03-18 20:30:18 -0300

    update setup.py to include package data

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(name='pycda',
-    version='0.1.11',
+    version='0.1.12',
     description='Python Crater Detection Algorithm (PyCDA) is a pipeline for crater detection; go from image to annotated crater stats in minutes.',
     url='https://github.com/AlliedToasters/PyCDA',
-    download_url='https://github.com/AlliedToasters/PyCDA/archive/0.1.11.tar.gz',
+    download_url='https://github.com/AlliedToasters/PyCDA/archive/0.1.12.tar.gz',
     keywords = ['crater detection', 'astronomy', 'planetary science', 'planetary geology'],
     author='Michael Klear',
     author_email='michael.klear@colorado.edu',
     license='MIT',
     packages=['pycda'],
     classifiers=[],
+    include_package_data=True,
     install_requires=[
         'numpy',
         'pandas',
