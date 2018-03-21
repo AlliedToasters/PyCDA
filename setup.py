2455b8b825d8cc3ec423d8a2d4bfa28ed53bc332
AlliedToasters <michael.r.klear@gmail.com>
2018-03-21 10:26:08 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -16,10 +16,18 @@ setup(name='pycda',
     classifiers=[],
     data_files = [
         'pycda/models/tinynet.h5',
+        'pycda/models/unet.h5',
         'pycda/models/classifier_12x12_2.h5',
         'pycda/sample_imgs/holdout_tile_labels.csv',
         'pycda/sample_imgs/holdout_tile.pgm',
+        'pycda/sample_imgs/holdout_tile_l.png',
         'pycda/sample_imgs/rgb_sample.jpg',
+        'pycda/sample_imgs/mercury.png',
+        'pycda/sample_imgs/selection0.png',
+        'pycda/sample_imgs/selection2.png',
+        'pycda/sample_imgs/selection3.png',
+        'pycda/sample_imgs/selection4.png',
+        'pycda/sample_imgs/selection5.png'
     ],
     include_package_data=True,
     install_requires=[
