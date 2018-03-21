3f1a331a2c8643d2f2611465049fcaba2176c034
AlliedToasters <michael.r.klear@gmail.com>
2018-03-21 13:02:09 -0300

    update docs for 0.1.14

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Michael Klear'
 # The short X.Y version
 version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.13'
+release = '0.1.14'
 
 
 # -- General configuration ---------------------------------------------------
