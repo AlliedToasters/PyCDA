2759b9301b517076b99f803bf348930938fae431
AlliedToasters <michael.r.klear@gmail.com>
2018-06-04 09:32:36 -0300

    resolve install dependency issue

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
-import pkg_resources
-
-path = pkg_resources.resource_filename('pycda', 'models/tinynet.h5')
 
 setup(name='pycda',
-    version='0.1.14',
+    version='0.1.15',
     description='Python Crater Detection Algorithm (PyCDA) is a pipeline for crater detection; go from image to annotated crater stats in minutes.',
     url='https://github.com/AlliedToasters/PyCDA',
-    download_url='https://github.com/AlliedToasters/PyCDA/archive/0.1.14.tar.gz',
+    download_url='https://github.com/AlliedToasters/PyCDA/archive/0.1.15.tar.gz',
     keywords = ['crater detection', 'astronomy', 'planetary science', 'planetary geology'],
     author='Michael Klear',
     author_email='michael.klear@colorado.edu',
